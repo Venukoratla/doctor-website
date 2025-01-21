42b422fcda3522decbc88685661fcfbe1363e768
venukoratla <koratlavenu09@gmail.com>
2025-01-21 22:58:54 +0530

    footer

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,6 +1,15 @@
 // Import necessary libraries
 import React from "react";
 import "./Footer.css"; // Import a separate CSS file for styling
+import logo from "../images/temp_logo.png";
+
+import {
+  FaFacebook,
+  FaInstagram,
+  FaWhatsapp,
+  FaYoutube,
+  FaLinkedin,
+} from "react-icons/fa";
 
 const Footer = () => {
   return (
@@ -12,8 +21,8 @@ const Footer = () => {
         {/* Content container */}
         <div className="footer-section footer-about">
           <img
-            src="/logo.png" // Replace with your logo path
-            alt="Shelter Logo"
+            src={logo} // Replace with your logo path
+            alt="Company Logo"
             className="footer-logo"
           />
           <p>
@@ -21,37 +30,28 @@ const Footer = () => {
             <br />
             The budgetary unit of the Capital City of Warsaw
           </p>
-          <div className="footer-socials">
-            <span className="social-icon">F</span>
-            <span className="social-icon">I</span>
-            <span className="social-icon">Y</span>
-          </div>
         </div>
         <div className="footer-section footer-links">
-          <h4>ANIMALS</h4>
+          <h4>Quick Links</h4>
           <ul>
-            <li>When your pet is missing</li>
-            <li>Recently found</li>
-            <li>How to adopt?</li>
-            <li>Pets for adoption</li>
-            <li>Material gifts</li>
-            <li>Help with walks</li>
-            <li>Volunteer activities</li>
+            <li>About Us</li>
+            <li>Services</li>
+            <li>Blogs</li>
+            <li>Contact us</li>
           </ul>
         </div>
         <div className="footer-section footer-info">
-          <h4>ANIMALS</h4>
-          <ul>
-            <li>General information</li>
-            <li>About the shelter</li>
-            <li>Statistic data</li>
-            <li>Job</li>
-            <li>Tenders</li>
-            <li>Contact</li>
+          <h4 style={{ textAlign: "center" }}>Follow Us</h4>
+          <ul className="footer-socials">
+            <FaFacebook size={32} className="footer-facebook" />
+            <FaInstagram size={32} className="footer-instagram" />
+            <FaWhatsapp size={32} className="footer-whatsapp" />
+            <FaYoutube size={32} className="footer-youtube" />
+            <FaLinkedin size={32} className="footer-linkedin" />
           </ul>
         </div>
         <div className="footer-section footer-contact">
-          <h4>CONTACT</h4>
+          <h4>Address</h4>
           <p>Warsaw, 57 Street, Poland</p>
           <p>+01 234 567 89</p>
           <p>contact@example.com</p>
